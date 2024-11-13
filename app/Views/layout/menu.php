d97af828932664ad834ae4a3d8d1b25254f5508f
dnightfall <dnfdika11@gmail.com>
2024-11-13 15:58:07 +0700

    menu

--- app/Views/layout/menu.php
+++ app/Views/layout/menu.php
@@ -1,11 +1,8 @@
-<li class="menu-header">Dashboard</li>
-              <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-                <ul class="dropdown-menu">
-                  <li><a class="nav-link" href=<?=site_url('akun1')?>>Akun</a></li>
-                  <li><a class="nav-link" href="<?=site_url('permintaanpembelian')?>">Permintaan</a></li>
-                  <li><a class="nav-link" href="<?=site_url('persetujuan')?>">Persetujuan</a></li>
-                  <li><a class="nav-link" href="<?=site_url('purchaseorder')?>">PO</a></li>
-                </ul>
-              </li>
+
+              <li class="menu-header">Dashboard</li>
+              <li class=""><a class="nav-link" href="<?=site_url('akun1')?>>Akun</a>"><i class="fas fa-solid fa-user"></i><span>Akun</span></a></li>
+              <li class=""><a class="nav-link" href="<?=site_url('permintaanpembelian')?>"><i class="fas fa-solid fa-box-open"></i><span>Permintaan</span></a></li>
+              <li class=""><a class="nav-link" href="<?=site_url('persetujuan')?>"><i class="fas fa-solid fa-handshake"></i><span>Persetujuan</span></a></li>
+              <li class=""><a class="nav-link" href="<?=site_url('purchaseorder')?>"><i class="fas fa-solid fa-truck"></i><span>PO</span></a></li>
+              
               
